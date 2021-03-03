d355617abab2f32316b7e6eb1af14546c4a77167
Bernd Fix <brf@hoi-polloi.org>
2021-03-03 15:11:06 +0100

    Removed obsolete script.

--- proto-gen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-protoc --go_out=. --go_opt=paths=source_relative pb.proto
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative grpc.proto
